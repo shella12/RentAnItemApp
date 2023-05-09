060d93628048fb6d48aef8434cdb5af31c2f43be
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-05-09 11:55:30 +0430

    Resolve the login part

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-import Registration from "../auth/Registration";
-import Login from "../auth/Login";
+import Registration from "./auth/Registration";
+import Login from "./auth/Login";
 
 export default class Home extends Component {
   constructor(props) {
