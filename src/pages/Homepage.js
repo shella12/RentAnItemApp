527d2972c8274e2daac90b2592da9593dd981a8f
shella12 <ayeshaarshad4567@gmail.com>
2023-05-10 18:31:17 +0500

    Update style

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,6 +3,7 @@ import { Carousel } from 'react-responsive-carousel';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchHouse } from '../redux/house/house';
+import styles from './house.module.css';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -13,14 +14,13 @@ const Home = () => {
 
   return (
     <>
-      <Carousel showArrows>
+      <Carousel showArrows autoFocus centerMode>
         {houses.map((house) => (
           <div key={house.id}>
-            <img src={house.picture} alt="" />
+            <img src={house.picture} alt="" className={styles.caroselImage} />
             <p className="legend">{house.name}</p>
           </div>
         )) }
-
       </Carousel>
     </>
   );
