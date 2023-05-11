a3d13184f518b568e87aee420c4e584fdaf45aea
Allassane <dickoallassane1997@gmail.com>
2023-05-11 10:05:35 +0100

    Fix linter errors

--- src/redux/favorites/favoriteReducer.js
+++ src/redux/favorites/favoriteReducer.js
@@ -1,61 +1,57 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 export const fetchFavorites = createAsyncThunk('rent-house/favorites', async (userID) => {
-  const result = await fetch(`http://localhost:3000/api/v1/users/${userID}/favorite_houses`)
-  return result.json()
-})
+  const result = await fetch(`http://localhost:3000/api/v1/users/${userID}/favorite_houses`);
+  return result.json();
+});
 
-export const postFavorite = createAsyncThunk('rent-house/favorite/Create', async ({userID, house}) => {
+export const postFavorite = createAsyncThunk('rent-house/favorite/Create', async ({ userID, house }) => {
   await fetch(`http://localhost:3000/api/v1/users/${userID}/favorite_houses/`, {
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     },
     method: 'POST',
     body: JSON.stringify({
-      "user_id": userID,
-      "house_id": house.id
-    })
-  })
-  return house
-})
+      user_id: userID,
+      house_id: house.id,
+    }),
+  });
+  return house;
+});
 
-export const deleteFavorite = createAsyncThunk('rent-house/favorite/Delete', async ({userID, houseID}) => {
+export const deleteFavorite = createAsyncThunk('rent-house/favorite/Delete', async ({ userID, houseID }) => {
   await fetch(`http://localhost:3000/api/v1/users/${userID}/favorite_houses/${houseID}/`, {
     method: 'DELETE',
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      "user_id": userID,
-      "house_id": houseID
-    })
-  })
-  return houseID
-})
+      user_id: userID,
+      house_id: houseID,
+    }),
+  });
+  return houseID;
+});
 
 const favoriteReducer = createSlice({
-  name: "favorites",
+  name: 'favorites',
   initialState: {
     favorites: [],
   },
   extraReducers: (builder) => {
     builder
-    .addCase(fetchFavorites.fulfilled, 
-      (state, action) => {
-        return ({...state, favorites: [...action.payload]})}
-    )
-    .addCase(postFavorite.fulfilled,
-      (state, action) => ({...state, favorites: [...state.favorites, action.payload]})
-    )
-    .addCase(deleteFavorite.fulfilled, 
-      (state, action) => {
-        const newFavorites = state.favorites.filter(favorite => favorite.id !== action.payload)
-        return ({...state, favorites: [...newFavorites]})
-      }
-    )
-  }
+      .addCase(fetchFavorites.fulfilled,
+        (state, action) => ({ ...state, favorites: [...action.payload] }))
+      .addCase(postFavorite.fulfilled,
+        (state, action) => ({ ...state, favorites: [...state.favorites, action.payload] }))
+      .addCase(deleteFavorite.fulfilled,
+        (state, action) => {
+          const newFavorites = state.favorites.filter((favorite) => favorite.id !== action.payload);
+          return ({ ...state, favorites: [...newFavorites] });
+        });
+  },
 });
 
 export default favoriteReducer.reducer;
