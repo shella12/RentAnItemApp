739333d8de6c86597ba1de9efd5c1b699b8727a2
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-05-14 11:23:48 +0430

    Add header for landing page

---
+++ src/componenets/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.css'
+import Navbar from './Navbar'
+
+
+export default function Header() {
+  return (
+    <header className="header">
+        <Navbar />
+
+      <div className="header-content mt-5">
+        <h1 className="header-content__title">Find perfect rides that fits </h1>
+        <p className="header-content__desc">
+          The best offers for you at any point of your journey.
+          Classy rides that meets your taste.
+        </p>
+        <p>
+          <Link className="btn btn-oval" to="/register">Hire Now</Link>
+        </p>
+      </div>
+    </header>
+  );
+}
