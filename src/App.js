b012e1baa3450d4a4488a9d86709871fbeb9d38e
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-05-14 11:23:31 +0430

    Add app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
+import Header from './componenets/Header';
 import Home from './pages/Homepage';
 import MyFavourites from './pages/MyFavourites';
 import AddHouse from './pages/AddHouse';
 import DeleteHouse from './pages/DeleteHouse';
 import HouseDetails from './pages/HouseDetails';
-import Navbar from './componenets/Navbar';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Header />
       <Routes>
+
         <Route path="/" element={<Home />} />
         <Route path="/MyFavourites" element={<MyFavourites />} />
         <Route path="/AddHouse" element={<AddHouse />} />
