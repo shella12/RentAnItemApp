0b4010afc152860bff9d0c7477ba42e445a60d0d
Allassane <dickoallassane1997@gmail.com>
2023-05-15 15:02:11 +0100

    Add test for App.js render

--- src/_test_/App.test.js
+++ src/_test_/App.test.js
@@ -5,22 +5,27 @@ import { Provider } from 'react-redux';
 import { fetchFavoriteHouseData, fetchHouseData, testListFavoriteHouses } from './setupTests';
 import configureStore from '../redux/configureStore';
 import MyFavourites from '../pages/MyFavourites';
-
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+import { fetchFavorites } from '../redux/favorites/favoriteReducer';
 
 const store = configureStore;
 fetchHouseData()
 fetchFavoriteHouseData(1);
 
-describe('test render', () => {
-  test('List Favorites should match snapshoot', () => {
+describe('test App render', () => {
+  test('App should match snapshoot', () => {
     const tree = renderer.create(
       <React.StrictMode>
         <Provider store={store}>
-          <MyFavourites />
+          <MemoryRouter initialEntries={['/']}>
+            <App />
+          </MemoryRouter>
         </Provider>
       </React.StrictMode>,
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
-});
+})
