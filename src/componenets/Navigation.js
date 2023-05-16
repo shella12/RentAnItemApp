c819f6e0fd3f9d664e0f42e5455313c7b8c8149e
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-05-16 14:56:20 +0430

    Add Navigation

--- src/componenets/Navigation.js
+++ src/componenets/Navigation.js
@@ -2,41 +2,29 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import logo from '../assets/profilePhoto.png';
+import logo from '../assets/red-circle-effect-png-transparent-5Kdj5f.png';
+import '../App.scss';
 
 export default function Navigation() {
-  const token = useSelector(state => state.user.token);
-  const username = useSelector(state => state.user.username);
+//   const token = useSelector(state => state.user.token);
+//   const email = useSelector(state => state.users.email);
 
   return (
-    <Navbar collapseOnSelect expand="sm">
-      <Navbar.Brand href="/"><img className="logo" src={logo} alt="Car Rentals" /></Navbar.Brand>
+    <Navbar collapseOnSelect expand="sm" className="custom-navbar">
+      <Navbar.Brand href="/"><img className="logo" src={logo} alt="" /></Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto" />
-        <Nav>
-          {
-            token === undefined || token === 'undefined'
-              ? (
+        <Nav className='auth_links'>
+         
                 <>
-                  <Link className="nav-link white" to="/login">SIGN IN</Link>
+                  <Link className="nav-link " to="/login">SIGN IN</Link>
+                  <span className="nav-link-space"></span>
                   <Link className="nav-link" to="/register">
                     <span className="btn-menu">SIGN UP</span>
                   </Link>
                 </>
-              )
-              : (
-                <>
-                  <Link className="nav-link white" to="/cars">CARS</Link>
-                  <Link className="nav-link white" to="/favourites">Favourite Cars</Link>
-                  <Link className="nav-link white" to="/logout">
-                    LOGOUT
-                    {' '}
-                    {email}
-                  </Link>
-                </>
-              )
-          }
+              
         </Nav>
       </Navbar.Collapse>
     </Navbar>
