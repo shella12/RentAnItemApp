a6d5ce091660313293caf514249538d62e13a97e
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-05-18 15:32:41 +0430

    Solve the linters error

--- src/Routess.js
+++ src/Routess.js
@@ -10,20 +10,18 @@ import App from './pages/App';
 // import FeatureSection from './componenets/FeatureSection';
 // import Footer from './componenets/Footer';
 
-const Routess = () => {
-  return (
-    <div className="App">
-      {/* <Navbar /> */}
-     
-      <Routes>
+const Routess = () => (
+  <div className="App">
+    {/* <Navbar /> */}
+
+    <Routes>
       <Route exact path="/" element={<App />} />
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/MyFavourites" element={<MyFavourites />} />
-        <Route path="/AddHouse" element={<AddHouse />} />
-        <Route path="/DeleteHouse" element={<DeleteHouse />} />
-        <Route path="/HouseDetails" element={<HouseDetails />} />
-      </Routes>
-    </div>
-  );
-  }
+      <Route path="/" element={<Home />} />
+      <Route path="/MyFavourites" element={<MyFavourites />} />
+      <Route path="/AddHouse" element={<AddHouse />} />
+      <Route path="/DeleteHouse" element={<DeleteHouse />} />
+      <Route path="/HouseDetails" element={<HouseDetails />} />
+    </Routes>
+  </div>
+);
 export default Routess;
--- src/componenets/Navigation.js
+++ src/componenets/Navigation.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import logo from '../assets/logo-fotor-bg-remover-2023051812511.png';
 
 export default function Navigation() {
-
   return (
     <Navbar collapseOnSelect expand="sm">
       <Navbar.Brand href="/"><img className="logo" src={logo} alt="circle" /></Navbar.Brand>
@@ -13,15 +12,14 @@ export default function Navigation() {
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto" />
         <Nav>
-       
-                <>
-                  <Link className="nav-link white " style={{fontSize: '1.3rem', fontWeight: '550'}} to="/login">SIGN IN</Link>
-                  <Link className="nav-link" style={{fontSize: '1.3rem', fontWeight: '500'}} to="/register">
-                    <span className="btn-menu">SIGN UP</span>
-                  </Link>
-                </>
-          
-              
+
+          <>
+            <Link className="nav-link white " style={{ fontSize: '1.3rem', fontWeight: '550' }} to="/login">SIGN IN</Link>
+            <Link className="nav-link" style={{ fontSize: '1.3rem', fontWeight: '500' }} to="/register">
+              <span className="btn-menu">SIGN UP</span>
+            </Link>
+          </>
+
         </Nav>
       </Navbar.Collapse>
     </Navbar>
