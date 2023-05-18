f76206578dc02d6b186ffb8efa3e66a01e3867e7
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-05-18 18:06:28 +0430

    Solve the conflict

--- src/Routess.js
+++ src/Routess.js
@@ -1,21 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
-// import Navbar from './componenets/Navbar';
 import Home from './pages/Homepage';
 import MyFavourites from './pages/MyFavourites';
 import AddHouse from './pages/AddHouse';
 import DeleteHouse from './pages/DeleteHouse';
-import HouseDetails from './pages/HouseDetails';
-// import Header from './componenets/Header';
 import App from './pages/App';
-// import FeatureSection from './componenets/FeatureSection';
-// import Footer from './componenets/Footer';
 import HouseDetails from './pages/HouseDetail/HouseDetails';
-// import Auth from './componenets/Auth';
 
 const Routess = () => {
   return (
     <div className="App">
-      {/* <Navbar /> */}
       <Routes>
       <Route exact path="/" element={<App />} />
         <Route path="houses">
