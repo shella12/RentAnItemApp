161b34dd192edaaa3c5dd7df69bf624012a28aa4
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-05-18 12:39:59 +0430

    Make Header Responsive

--- src/componenets/Header.js
+++ src/componenets/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../App.scss';
 import Navigation from './Navigation';
 
 export default function Header() {
@@ -9,13 +8,13 @@ export default function Header() {
       <Navigation />
 
       <div className="header-content mt-5">
-        <h1 className="header-content__title">Find housing anywhere</h1>
+        <h1 className="header-content__title">Find perfect rides that fits </h1>
         <p className="header-content__desc">
           The best offers for you at any point of your journey.
-          where ever you are, feel yourself like at your own home
+          Classy rides that meets your taste.
         </p>
         <p>
-          <Link className="btn btn-oval" to="/">Hire Now</Link>
+          <Link className="btn btn-oval" to="/register">Hire Now</Link>
         </p>
       </div>
     </header>
