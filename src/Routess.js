67468b977f73ac0f0e77596f4d02ad789a6d3952
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-05-18 12:41:10 +0430

    Change App.js to Routess.js

---
+++ src/Routess.js
@@ -0,0 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
+// import Navbar from './componenets/Navbar';
+import Home from './pages/Homepage';
+import MyFavourites from './pages/MyFavourites';
+import AddHouse from './pages/AddHouse';
+import DeleteHouse from './pages/DeleteHouse';
+import HouseDetails from './pages/HouseDetails';
+// import Header from './componenets/Header';
+import App from './pages/App';
+// import FeatureSection from './componenets/FeatureSection';
+// import Footer from './componenets/Footer';
+
+const Routess = () => {
+  return (
+    <div className="App">
+      {/* <Navbar /> */}
+     
+      <Routes>
+      <Route exact path="/" element={<App />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/MyFavourites" element={<MyFavourites />} />
+        <Route path="/AddHouse" element={<AddHouse />} />
+        <Route path="/DeleteHouse" element={<DeleteHouse />} />
+        <Route path="/HouseDetails" element={<HouseDetails />} />
+      </Routes>
+    </div>
+  );
+  }
+export default Routess;
