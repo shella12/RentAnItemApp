0ab97d58f7b73776c1c6d9a9a1466b5d1acbef43
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-05-20 11:50:49 +0430

    Solve the bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
+import React, { useCallback } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 import Registration from './auth/Registration';
 import Login from './auth/Login';
 
-export default class Home extends Component {
-  constructor(props) {
-    super(props);
+const Home = ({ handleLogin, handleLogout, loggedInStatus }) => {
+  const navigate = useNavigate();
 
-    this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-  }
-
-  handleSuccessfulAuth(data) {
-    const { handleLogin, history } = this.props;
+  const handleSuccessfulAuth = useCallback((data) => {
     handleLogin(data);
-    history.push('/dashboard');
-  }
+    navigate('/dashboard');
+  }, [handleLogin, navigate]);
 
-  handleLogoutClick() {
-    const { handleLogout } = this.props;
+  const handleLogoutClick = useCallback(() => {
     axios
       .delete('http://localhost:3001/logout', { withCredentials: true })
       .then(() => {
@@ -29,32 +23,28 @@ export default class Home extends Component {
       .catch((error) => {
         console.log('logout error', error);
       });
-  }
+  }, [handleLogout]);
 
-  render() {
-    const { loggedInStatus } = this.props;
-    return (
-      <div>
-        <h1>Home</h1>
-        <h1>
-          Status:
-          {loggedInStatus}
-        </h1>
-        <button type="button" onClick={() => this.handleLogoutClick()}>
-          Logout
-        </button>
-        <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
-        <Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Home</h1>
+      <h1>
+        Status:
+        {loggedInStatus}
+      </h1>
+      <button type="button" onClick={handleLogoutClick}>
+        Logout
+      </button>
+      <Registration handleSuccessfulAuth={handleSuccessfulAuth} />
+      <Login handleSuccessfulAuth={handleSuccessfulAuth} />
+    </div>
+  );
+};
 
 Home.propTypes = {
   handleLogin: PropTypes.func.isRequired,
   handleLogout: PropTypes.func.isRequired,
   loggedInStatus: PropTypes.string.isRequired,
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired,
-  }).isRequired,
 };
+
+export default Home;
